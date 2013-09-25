3f9d7b8e43ab8eaa1715a76703ad2de3b1974ce8
Potfur <wachowski.michal@gmail.com>
2013-09-25 23:38:28 +0200

    implemented

--- tests/moss/view/ViewTest.php
+++ tests/moss/view/ViewTest.php
@@ -1,21 +1,134 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: Michal
- * Date: 27.07.13
- * Time: 16:13
- * To change this template use File | Settings | File Templates.
- */
-
 namespace tests\moss\view;
 
+use moss\view\View;
 
 class ViewTest extends \PHPUnit_Framework_TestCase
 {
 
-    public function testNotImplemented()
+    public function testTemplate()
     {
-        $this->markTestIncomplete();
+        $View = new View($this->getTwigMock());
+        $View->template('foo');
+
+        $result = array(
+            'foo',
+            array()
+        );
+        $this->assertEquals($result, $View->render());
     }
 
+    /**
+     * @dataProvider setProvider
+     */
+    public function testSet($result, $key, $value = null)
+    {
+        $View = new View($this->getTwigMock());
+        $View
+            ->template('foo')
+            ->set($key, $value);
+
+        $this->assertEquals(array('foo', $result), $View->render());
+    }
+
+    public function setProvider()
+    {
+        return array(
+            array(
+                array('a' => null),
+                'a'
+            ),
+            array(
+                array('b' => 'c'),
+                'b',
+                'c'
+            ),
+            array(
+                array('d', 'e'),
+                array('d', 'e')
+            ),
+            array(
+                array('f' => array('g', 'h')),
+                'f',
+                array('g', 'h')
+            ),
+            array(
+                array('i' => 'j', 'k' => 'l'),
+                array('i' => 'j', 'k' => 'l')
+            ),
+            array(
+                array('m' => array('n' => 'o')),
+                'm',
+                array('n' => 'o')
+            ),
+            array(
+                array('m' => array('n' => 'o')),
+                'm.n',
+                'o'
+            )
+        );
+    }
+
+    /**
+     * @dataProvider getProvider
+     */
+    public function testGet($result, $name, $key, $value = null)
+    {
+        $View = new View($this->getTwigMock());
+        $View
+            ->template('foo')
+            ->set($key, $value);
+
+        $this->assertEquals($result, $View->get($name));
+    }
+
+    public function getProvider()
+    {
+        return array(
+            array(null, 'a', 'a'),
+            array('c', 'b', 'b', 'c'),
+            array(0, 'd', array('d', 'e')),
+            array(array('g', 'h'), 'f', 'f', array('g', 'h')),
+            array('j', 'i', array('i' => 'j', 'k' => 'l')),
+            array('o', 'm.n', 'm', array('n' => 'o')),
+            array('o', 'm.n', array('m' => array('n' => 'o')))
+        );
+    }
+
+    public function testRender()
+    {
+        $View = new View($this->getTwigMock());
+        $result = $View
+            ->template('foo')
+            ->render();
+
+        $this->assertEquals(array('foo', array()), $result);
+    }
+
+    public function testToString()
+    {
+        $View = new View($this->getTwigMock());
+        $result = $View
+            ->template('foo')
+            ->__toString();
+
+        $this->assertEquals('Array', $result);
+    }
+
+    public function getTwigMock()
+    {
+        $Twig = $this->getMock('\Twig_Environment');
+        $Twig
+            ->expects($this->any())
+            ->method('render')
+            ->will(
+                $this->returnCallback(
+                    function () {
+                        return func_get_args();
+                    }
+                )
+            );
+
+        return $Twig;
+    }
 }
