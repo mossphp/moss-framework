d11fd3721f8bd00636dc604219846707a4b064aa
Potfur <wachowski.michal@gmail.com>
2013-10-14 20:32:10 +0200

    new amazing exception

---
+++ moss/security/TokenException.php
@@ -0,0 +1,15 @@
+<?php
+namespace moss\security;
+
+use moss\security\AuthenticationException;
+
+/**
+ * Token exception
+ *
+ * @package Moss Security
+ * @author  Michal Wachowski <wachowski.michal@gmail.com>
+ */
+class TokenException extends AuthenticationException
+{
+
+}
