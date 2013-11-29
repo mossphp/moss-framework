6ed36e07d7c2ca8e628965d1709230cc1dd726c0
Potfur <wachowski.michal@gmail.com>
2013-11-30 00:51:25 +0100

    crypt

---
+++ moss/security/CryptInterface.php
@@ -0,0 +1,37 @@
+<?php
+namespace moss\security;
+
+/**
+ * Security crypt interface
+ *
+ * @package Moss Security
+ * @author  Michal Wachowski <wachowski.michal@gmail.com>
+ */
+interface CryptInterface
+{
+    const SDES = 'sdes';
+    const EDES = 'edes';
+    const MD5 = 'md5';
+    const BLOWFISH = 'blowfish';
+    const SHA256 = 'sha256';
+    const SHA512 = 'sha512';
+
+    /**
+     * Crypts passed string with set algorithm
+     *
+     * @param string $password
+     *
+     * @return string
+     */
+    public function hash($password);
+
+    /**
+     * Returns true if password and hashed are equal
+     *
+     * @param $password
+     * @param $hashed
+     *
+     * @return bool
+     */
+    public function compare($password, $hashed);
+}
