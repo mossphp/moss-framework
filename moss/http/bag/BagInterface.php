2a32ce26be3ba63bd050e5c9f3196acc11f046dd
Potfur <wachowski.michal@gmail.com>
2013-12-04 02:09:50 +0100

    parameter bag

---
+++ moss/http/bag/BagInterface.php
@@ -0,0 +1,69 @@
+<?php
+namespace moss\http\bag;
+
+/**
+ * Parameter bag interface
+ *
+ * @package  Moss HTTP
+ * @author   Michal Wachowski <wachowski.michal@gmail.com>
+ */
+interface BagInterface extends \ArrayAccess, \Iterator, \Countable, \Serializable
+{
+    const SEPARATOR = '.';
+
+    /**
+     * Retrieves offset value
+     *
+     * @param string $offset
+     * @param mixed  $default
+     *
+     * @return mixed
+     */
+    public function get($offset = null, $default = null);
+
+    /**
+     * Sets value to offset
+     *
+     * @param string $offset
+     * @param mixed  $value
+     *
+     * @return $this
+     */
+    public function set($offset, $value = null);
+
+    /**
+     * Returns true if offset exists in bag
+     *
+     * @param string $offset
+     *
+     * @return bool
+     */
+    public function has($offset);
+
+    /**
+     * Removes offset from bag
+     * If no offset set, removes all values
+     *
+     * @param string $offset attribute to remove from
+     *
+     * @return $this
+     */
+    public function remove($offset = null);
+
+    /**
+     * Returns all options
+     * If array passed, becomes bag content
+     *
+     * @param array $array overwrites values
+     *
+     * @return array
+     */
+    public function all($array = array());
+
+    /**
+     * Removes all options
+     *
+     * @return $this
+     */
+    public function reset();
+}
