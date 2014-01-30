81fb7f2ef3074d95c1fdc61d7af514e3c8ada170
Potfur <wachowski.michal@gmail.com>
2014-01-30 19:44:44 +0100

    stub is part of the test

--- tests/moss/dispatcher/ListenerTest.php
+++ tests/moss/dispatcher/ListenerTest.php
@@ -1,6 +1,20 @@
 <?php
 namespace moss\dispatcher;
 
+class Foobar
+{
+    public $args;
+
+    public function __construct()
+    {
+        $this->args = func_get_args();
+    }
+
+    public function foo()
+    {
+        $this->args = func_get_args();
+    }
+}
 
 class ListenerTest extends \PHPUnit_Framework_TestCase
 {
@@ -9,25 +23,25 @@ class ListenerTest extends \PHPUnit_Framework_TestCase
     {
         $container = $this->getContainerMock();
 
-        $listener = new Listener('\tests\moss\Foobar', null, array());
-        $this->assertEquals(new \tests\moss\Foobar(), $listener->get($container));
+        $listener = new Listener('\moss\dispatcher\Foobar', null, array());
+        $this->assertEquals(new \moss\dispatcher\Foobar(), $listener->get($container));
     }
 
     public function testGetNoArgs()
     {
         $container = $this->getContainerMock();
 
-        $listener = new Listener('\tests\moss\Foobar', 'foo', array());
-        $this->assertEquals(new \tests\moss\Foobar(), $listener->get($container));
+        $listener = new Listener('\moss\dispatcher\Foobar', 'foo', array());
+        $this->assertEquals(new \moss\dispatcher\Foobar(), $listener->get($container));
     }
 
     public function testGetNormalArgs()
     {
         $container = $this->getContainerMock();
 
-        $listener = new Listener('\tests\moss\Foobar', 'foo', array('foo', 'bar', array('y', 'a', 'd', 'a')));
+        $listener = new Listener('\moss\dispatcher\Foobar', 'foo', array('foo', 'bar', array('y', 'a', 'd', 'a')));
         $this->assertEquals(
-            new \tests\moss\Foobar('foo', 'bar', array('y', 'a', 'd', 'a')), $listener->get($container)
+            new \moss\dispatcher\Foobar('foo', 'bar', array('y', 'a', 'd', 'a')), $listener->get($container)
         );
     }
 
@@ -35,8 +49,8 @@ class ListenerTest extends \PHPUnit_Framework_TestCase
     {
         $container = $this->getContainerMock();
 
-        $listener = new Listener('\tests\moss\Foobar', 'foo', array('@Subject', '@Message'));
-        $this->assertEquals(new \tests\moss\Foobar('Subject', 'Message'), $listener->get($container, 'Subject', 'Message'));
+        $listener = new Listener('\moss\dispatcher\Foobar', 'foo', array('@Subject', '@Message'));
+        $this->assertEquals(new \moss\dispatcher\Foobar('Subject', 'Message'), $listener->get($container, 'Subject', 'Message'));
     }
 
     protected function getContainerMock()
@@ -45,7 +59,7 @@ class ListenerTest extends \PHPUnit_Framework_TestCase
         $container
             ->expects($this->any())
             ->method('get')
-            ->will($this->returnValue(new \tests\moss\Foobar()));
+            ->will($this->returnValue(new \moss\dispatcher\Foobar()));
 
         return $container;
     }
@@ -56,14 +70,22 @@ class ListenerTest extends \PHPUnit_Framework_TestCase
         $container
             ->expects($this->at(0))
             ->method('get')
-            ->will($this->returnValue(new \tests\moss\Foobar()));
+            ->will($this->returnValue(new \moss\dispatcher\Foobar()));
 
         $container
             ->expects($this->at(1))
             ->method('get')
             ->will($this->returnValue(new \stdClass()));
 
-        $listener = new Listener('\tests\moss\Foobar', 'foo', array('@Foobar', '@Container'));
-        $this->assertEquals(new \tests\moss\Foobar(new \stdClass(), $container), $listener->get($container));
+        $listener = new Listener('\moss\dispatcher\Foobar', 'foo', array('@Foobar', '@Container'));
+        $this->assertEquals(new \moss\dispatcher\Foobar(new \stdClass(), $container), $listener->get($container));
+    }
+
+    public function testCallable()
+    {
+        $container = $this->getContainerMock();
+
+        $listener = new Listener('\moss\dispatcher\Foobar', 'foo', array('foo', 'bar', array('y', 'a', 'd', 'a')));
+        $this->assertEquals($listener($container), $listener->get($container));
     }
 }
