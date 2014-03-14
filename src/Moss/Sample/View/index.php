465c98ca9250619929eb8e6df10fd44b9e9475bf
Potfur <wachowski.michal@gmail.com>
2014-03-14 21:07:27 +0100

    added twig templates for moss-bridge demo

--- src/Moss/Sample/View/index.php
+++ src/Moss/Sample/View/index.php
@@ -9,9 +9,21 @@
     </style>
 </head>
 <body>
-<h1><?= $method ?> <small>&lt;-- this is namespaced controller class and its currently executed action</small></h1>
+<h1><?= $method ?>
+    <small>&lt;-- this is namespaced controller class and its currently executed action</small>
+</h1>
 
 <p>Moss sample controller and <a href="<?= $url('Moss:Sample:Sample:source') ?>">it looks like this</a></p>
+
+<p>Sample bundle uses <em>plain PHP</em> templates by default, but also includes
+    <a href="http://twig.sensiolabs.org/">Twig</a> templates for <var>moss/bridge</var></p>
+
+<p>This can be easily changed, just add
+    <var>"moss/bridge": "*"</var> to required packages in composer and install/update dependencies.</p>
+
+<p>Last step is to replace default
+    <em>view</em> definition in bootstrap with that in bridges readme.<br>No further changes in controllers need to be made.
+</p>
 
 </body>
 </html>
